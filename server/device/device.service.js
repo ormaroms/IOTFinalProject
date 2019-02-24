aa898bc3f51b135233a4295b10ccd8017a4ae740
Or Marom <orrmaromm@gmail.com>
2019-02-24 20:59:29 +0200

    fixing result in getting all users without __v and password properties

--- server/device/device.service.js
+++ server/device/device.service.js
@@ -2,12 +2,11 @@ const db = require('_helpers/db');
 const Device = db.Device;
 
 async function getById(id) {
-    return await Device.findOne({ userId: id }).then(result => {return {"devices": result.devices}});
+    return await Device.findOne({ userId: id }, {_id: 0}).then(result => {return {"devices": result.devices}});
 }
 
 async function create(id ,deviceParam) {
     if (await Device.findOne({ userId: id })) {
-        //throw 'User Id with the Id "' + id + '" is already taken';
         await Device.remove({ userId: id});
 
     }
@@ -15,7 +14,7 @@ async function create(id ,deviceParam) {
 
         await device.save();
 
-    return await Device.find({userId: id}).then(result => {return {"devices": result.devices}});
+    return await Device.find({userId: id}, {_id: 0}).then(result => {return {"devices": result.devices}});
 }
 
 async function update(id, deviceParam) {
--- server/users/user.service.js
+++ server/users/user.service.js
@@ -26,11 +26,11 @@ async function authenticate({ username, password }) {
 }
 
 async function getAll() {
-    return await User.find().select('-password');
+    return await User.find({}, {password: 0, __v: 0});
 }
 
 async function getById(id) {
-    return await User.findById(id).select('-password');
+    return await User.findById(id, {password: 0, __v: 0});
 }
 
 async function create(userParam) {
