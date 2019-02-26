18eb5ac64547c2ff11036bb3dd046d22cedbe2b8
Yoni Solimanov <rtyu28@gmail.com>
2019-02-26 17:58:06 +0200

    Working poc

--- client/src/reducers/status.js
+++ client/src/reducers/status.js
@@ -1,13 +1,13 @@
-import {STATUS_DATA_FETCHED} from '../actions/status'
+
 const defaultState = {
-    isGasLit: false,
-    isGasLeaking: true,
-    arduinoID: 1,
+    lightStatus: false,
+    gasStatus: false,
+    arduinoID: "1",
 }
 
 export default function reduce(state = defaultState, action) {
     switch (action.type) {
-        case STATUS_DATA_FETCHED:
+        case "STATUS_DATA_FETCHED":
             return Object.assign({}, action.data)
         default: return state
     }
--- client/src/serverapi/index.js
+++ client/src/serverapi/index.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 const SERVER = 'http://localhost:4000'
 
 export function getStatus(id) {
-    return axios.get(SERVER + '/status/' + id)
+    return axios.get(SERVER + '/status/' + id, {params: {id: id}})
 }
