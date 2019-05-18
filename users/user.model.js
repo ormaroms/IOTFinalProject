5480d9eecca61dd701e9c23482414d84b765ff8d
Or Marom <orrmaromm@gmail.com>
2019-05-18 17:06:35 +0300

    adding email and name

--- users/user.model.js
+++ users/user.model.js
@@ -3,6 +3,8 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
     username: { type: String, unique: true, required: true },
+    name: { type: String },
+    email: { type: String, unique: true },
     password: { type: String, required: true },
     createdDate: { type: Date, default: Date.now }
 });
