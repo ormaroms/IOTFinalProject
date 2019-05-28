5c59b537aa364ff9e268eea2a73b0c29f49fb5dd
Yoni Solimanov <rtyu28@gmail.com>
2019-05-28 17:15:53 +0300

    changed build path

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(cors());
 
 app.use(favicon(__dirname + '/client_build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'client_build')));
+app.use('/monitor', express.static(__dirname));
+app.use( '/monitor', express.static(path.join(__dirname, 'client_build')));
 app.get('/monitor', function (req, res) {
     res.sendFile(path.join(__dirname, 'client_build', 'index.html'));
 });
